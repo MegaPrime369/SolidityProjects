c562cfcc0875cff34ed657722424d4d24befe7ef
MegaPrime369 <megaprime0207@gmail.com>
2024-01-02 09:56:59 +0530

    Created the Simple Storage Contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,1 +1,13 @@
 //SPDX-License-Identifier:- Unlicensed
+pragma solidity >=0.4.0 < 0.9.0;
+
+// A simple contract to change the state of a variable
+contract SimpleStorage{
+    uint storedData;
+    function set(uint x) public{
+        storedData = x;
+    }
+    function get() public view returns(uint){
+        return storedData;
+    }
+}
